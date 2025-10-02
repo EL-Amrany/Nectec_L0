cbfb7667178450158e68950dd60b8737b52c3c47
EL-Amrany <105726186+EL-Amrany@users.noreply.github.com>
2025-10-02 14:34:27 +0200

    Update run.py

--- run.py
+++ run.py
@@ -7,4 +7,4 @@ if __name__ == "__main__":
     # Use the PORT environment variable provided by Render
     port = int(os.environ.get("PORT", 8080))
     # Bind to 0.0.0.0 so the port is reachable
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0",debug=True)
