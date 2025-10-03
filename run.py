88e01cb337dd79576159b0e340ae5c3edee2551e
EL-Amrany <105726186+EL-Amrany@users.noreply.github.com>
2025-10-03 18:12:18 +0200

    Update run.py

--- run.py
+++ run.py
@@ -1,16 +1,9 @@
-# run.py
-from flask import Flask
-# if you keep routes in routes.py:
-# from routes import bp as routes_bp
+from app import create_app
 
-app = Flask(__name__)
-# app.register_blueprint(routes_bp)
-
-@app.route("/")
-def home():
-    return "OK from Flask on Vercel"
+app = create_app()
 
 if __name__ == "__main__":
-    # This should NOT execute on Vercel import
     app.run()
+
+
 
